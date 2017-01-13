5b79fba32590c185a7ac5df300d826ba9b585a50
friedpine <temp@yh-imac.local>
2017-01-13 10:01:33 +0800

    modify bash shell

--- run_cnv_qsub.sh
+++ run_cnv_qsub.sh
@@ -1,2 +1,3 @@
-echo "bash /mnt/gpfs/Database/softs/CNV_V01/s1_server.sh $2 /mnt/gpfs/Database/process/CNV/$1 $3 $1 /mnt/gpfs/Database/softs/CNV_V01" >"./scripts/wk_$1.sh"
-qsub -cwd -l vf=6g ./scripts/wk_$1.sh
+cd ./scripts
+echo "bash /mnt/gpfs/Database/softs/CNV_V01/s1_server.sh $2 /mnt/gpfs/Database/process/CNV/$1 $3 $1 /mnt/gpfs/Database/softs/CNV_V01" >"./wk_$1.sh"
+qsub -cwd -l vf=6g ./wk_$1.sh
