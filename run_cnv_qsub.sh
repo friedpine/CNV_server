346a23a92d443a7a775cf50c36190cde38d3109e
friedpine <temp@yh-imac.local>
2017-01-13 10:10:38 +0800

    run local s1 script..

--- run_cnv_qsub.sh
+++ run_cnv_qsub.sh
@@ -1,3 +1,3 @@
 cd ./scripts
-echo "bash /mnt/gpfs/Database/softs/CNV_V01/s1_server.sh $2 /mnt/gpfs/Database/process/CNV/$1 $3 $1 /mnt/gpfs/Database/softs/CNV_V01" >"./wk_$1.sh"
+echo "bash ../s1_server.sh $2 /mnt/gpfs/Database/process/CNV/$1 $3 $1 /mnt/gpfs/Database/softs/CNV_V01" >"./wk_$1.sh"
 qsub -cwd -l vf=6g ./wk_$1.sh
