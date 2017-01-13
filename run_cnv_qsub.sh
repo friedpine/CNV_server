53726cf2f5cbab7d1a86a7e46b010f455e980a3a
friedpine <temp@yh-imac.local>
2017-01-13 09:12:25 +0800

    run_cnv_qsub

--- run_cnv_qsub.sh
+++ run_cnv_qsub.sh
@@ -1,3 +1,2 @@
-echo $1
-echo $2
-echo $3
+echo "bash /mnt/gpfs/Database/softs/CNV_V01/s1_server.sh $2 /mnt/gpfs/Database/process/CNV/$1 $3 $1 /mnt/gpfs/Database/softs/CNV_V01" >"./scripts/wk_$1.sh"
+qsub -cwd -l vf=6g ./scripts/wk_$1.sh
